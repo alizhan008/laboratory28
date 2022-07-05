60561cdfe0b40facf61f03744c0a5faa8372ada7
Alizhan Mametov <mametovalizhan@gmail.com>
2022-07-05 17:41:47 +0600

    task1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-// не хватило времени сделать до конца, болел.
+// не хватило времени реализовать до конца, болел.
     }
 }
